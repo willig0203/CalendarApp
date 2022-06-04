52e6d49dc09a22dd7771b19e32f8617e19cf7f68
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-06-03 21:46:36 -0500

    working on colors of time blocks

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,7 +5,7 @@ var containerDivEl = document.querySelector("#containerDiv");
 function getCurrDateAndTime() {
     var now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
     currentDayEl.textContent = now;
-}
+};
 
 var hoursArry =
     [
@@ -20,35 +20,119 @@ var hoursArry =
         "5pm"
     ];
 
-function addHours() {
+
+// // save in tasks array
+// tasks.toDo.push({
+//   text: taskText,
+//   date: taskDate
+// });
+
+// saveTasks();
+
+
+function makeDayDisplay() {
     for (var i = 0; i < hoursArry.length; i++) {
-
-        var hour = document.createElement("div");
-        hour.id = "hour" + hoursArry[i];
-        hour.className = "col-2";
-        hour.textContent = "hour";
-        containerDivEl.appendChild(hour);
-
-        var calendarEvent = document.createElement("div");
-        calendarEvent.id = "calendarEvent" + hoursArry[i];
-        calendarEvent.className = "col-8";
-        calendarEvent.textContent = "calendarEvent";
-        containerDivEl.appendChild(calendarEvent);
-
-        var saveEvent = document.createElement("button");
-        saveEvent.type = "button";
-        saveEvent.id = "saveEvent" + hoursArry[i];
-        saveEvent.className = "col-2";
-        saveEvent.textContent = "saveEvent";
-        containerDivEl.appendChild(saveEvent);
+        //hour
+        var taskLi = $("<li>").addClass("hour list-group-item");
+        var taskP = $("<p>").addClass("m-1").text(hoursArry[i]);
+        taskLi.append(taskP);
+        $("#list-c1").append(taskLi);
+        //event
+        var taskLi = $("<li>").addClass("list-group-item");
+        var taskP = $("<p>").addClass("m-1").text("event");
+        taskLi.append(taskP);
+        $("#list-c2").append(taskLi);
+        // save
+        var taskLi = $("<li>").addClass("saveBtn list-group-item");
+        var taskP = $("<label>").addClass("m-1").text("s");
+        taskLi.append(taskP);
+        $("#list-c3").append(taskLi);
     }
-}
-
-function saveEvent (event) {
-    console.log(event);
 };
 
-containerDivEl.addEventListener("click", saveEvent());
+$(".list-group").on("click", "label", function () {    
+    console.log("save button");
+});
 
-addHours();
+// task text was clicked
+$(".list-group").on("click", "p", function () {
+    // get current text of p element
+    var text = $(this)
+      .text()
+      .trim();
+  
+    // replace p element with a new textarea
+    var textInput = $("<textarea>").addClass("form-control").val(text);
+    $(this).replaceWith(textInput);
+  
+    // auto focus new element
+    textInput.trigger("focus");
+  });
+  
+  // editable field was un-focused
+  $(".list-group").on("blur", "textarea", function () {
+    // get current value of textarea
+    var text = $(this).val();
+  
+    // get status type and position in the list
+    var status = $(this)
+      .closest(".list-group")
+      .attr("id")
+      .replace("list-", "");
+    var index = $(this)
+      .closest(".list-group-item")
+      .index();
+  
+    // // update task in array and re-save to localstorage
+    // tasks[status][index].text = text;
+    // saveTasks();
+  
+    // recreate p element
+    var taskP = $("<p>")
+      .addClass("m-1")
+      .text(text);
+  
+    // replace textarea with new content
+    $(this).replaceWith(taskP);
+  });
+
+
+var updateListTimeColors = function () {
+
+    debugger
+    // current time
+    var now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    console.log(now);
+
+    // list and childer items
+    var itemListToColor = document.querySelector("#list-c2");
+    // each child gets a chance
+    for (var li of itemListToColor.children) {
+        console.log(li);
+    }
+
+    // we compare the times
+
+
+
+    // $(taskEl).removeClass("hour list-group-item past present future");
+
+    // if (moment().isAfter(time)) {
+    //   $(taskEl).addClass("list-group-item-danger");
+    // }
+    // else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    //   $(taskEl).addClass("list-group-item-warning");
+    // }
+};
+
+setInterval(function () {
+    $(".card .list-group-item").each(function (index, el) {
+        updateListTimeColors(el);
+    });
+}, (1000 * 60) * .25); // 1/4 min
+
+
+
+makeDayDisplay();
 getCurrDateAndTime();
+
