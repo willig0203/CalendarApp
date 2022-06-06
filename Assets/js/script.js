1e01cb45936e663c08d54154b0e7bf7907be5d88
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-06-05 23:05:27 -0500

    still working

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,15 +9,15 @@ function getCurrDateAndTime() {
 
 var hoursArry =
     [
-        "9 am",
-        "10 am",
-        "11 am",
-        "12 pm",
-        "1 pm",
-        "2 pm",
-        "3 pm",
-        "4 pm",
-        "5 pm"
+        "9am",
+        "10am",
+        "11am",
+        "12pm",
+        "1pm",
+        "2pm",
+        "3pm",
+        "4pm",
+        "5pm"
     ];
 
 
@@ -52,7 +52,7 @@ function makeDayDisplay() {
     }
 };
 
-$(".list-group").on("click", "label", function () {    
+$(".list-group").on("click", "label", function () {
     console.log("save button");
 });
 
@@ -60,70 +60,94 @@ $(".list-group").on("click", "label", function () {
 $(".list-group").on("click", "p", function () {
     // get current text of p element
     var text = $(this)
-      .text()
-      .trim();
-  
+        .text()
+        .trim();
+
     // replace p element with a new textarea
     var textInput = $("<textarea>").addClass("form-control").val(text);
     $(this).replaceWith(textInput);
-  
+
     // auto focus new element
     textInput.trigger("focus");
-  });
-  
-  // editable field was un-focused
-  $(".list-group").on("blur", "textarea", function () {
+});
+
+// editable field was un-focused
+$(".list-group").on("blur", "textarea", function () {
     // get current value of textarea
     var text = $(this).val();
-  
+
     // get status type and position in the list
     var status = $(this)
-      .closest(".list-group")
-      .attr("id")
-      .replace("list-", "");
+        .closest(".list-group")
+        .attr("id")
+        .replace("list-", "");
     var index = $(this)
-      .closest(".list-group-item")
-      .index();
-  
+        .closest(".list-group-item")
+        .index();
+
     // // update task in array and re-save to localstorage
     // tasks[status][index].text = text;
     // saveTasks();
-  
+
     // recreate p element
     var taskP = $("<p>")
-      .addClass("m-1")
-      .text(text);
-  
+        .addClass("m-1")
+        .text(text);
+
     // replace textarea with new content
     $(this).replaceWith(taskP);
-  });
+});
 
+debugger
+var updateListTimeColors = function (nowTestTime) {
 
-var updateListTimeColors = function () {
-
-    debugger
     // current time
-    var nowTime = moment().format("h a");  
+    //   var nowTime = moment().format("ha");
 
-    debugger
     // list and children items
     var itemListToColor = document.querySelector("#list-c2");
 
-    // each child 
-// 9 to 5  = 0-8
+    var clrCntr = 0;
 
-    var clrCntr= 0;
     for (var li of itemListToColor.children) {
 
-        console.log( nowTime + " '' " +  hoursArry[clrCntr] + " .. " + li);
+        debugger
 
+        if (nowTime.includes('am') && hoursArry[clrCntr].includes('am')) {
+
+            console.log('good morning');
+
+            var now = nowTime.replace('am', '');
+            var hrs = hoursArry[clrCntr].replace('am', '');
+
+            if (now === 12 || now < 9) {
+                console.log('all in future'); // color all green
+                continue;
+            }
+            if (now === hrs) {
+                console.log('present'); // red
+            }
+            if (now > hrs) {
+                console.log('past'); // grey
+            }
+        };
+        if (nowTime.includes('pm') && hoursArry[clrCntr].includes('pm')) {
+
+            console.log('good afternoon');
+
+            var now = nowTime.replace('pm', '');
+            var hrs = hoursArry[clrCntr].replace('pm', '');
+
+            if (now === 12) {
+                console.log('present');
+            }
+            if (now > hrs) {
+                console.log('future');
+            }
+        };
         clrCntr++;
-    }
-
-    
+    };
     debugger
-
-
 
     // $(taskEl).removeClass("hour list-group-item past present future");
 
@@ -135,11 +159,11 @@ var updateListTimeColors = function () {
     // }
 };
 
-setInterval(function () {
-    $(".card .list-group-item").each(function (index, el) {
-        updateListTimeColors(el);
-    });
-}, (1000 * 60) * .25); // 1/4 min
+// setInterval(function () {
+//     $(".card .list-group-item").each(function (index, el) {
+//         updateListTimeColors(el);
+//     });
+// }, (1000 * 60) * .25); // 1/4 min
 
 
 
